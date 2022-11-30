50c7235ec371c68976bc0810ad49b1fa9c32d0e1
Francesca <francesca.nadel@gmail.com>
2022-11-30 15:10:27 +0100

    Delete src2 directory

--- src2/app/models/card.model.ts
+++
@@ -1,9 +0,0 @@
-export class Card {    
-    constructor(
-        public thumbnail: string,
-        public title: string,
-        public description: string,
-        public btnText: string,
-        public isBtnDifferent: boolean
-    ) {}
-}
